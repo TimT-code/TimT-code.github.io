70b8e0d9bc5480e1152489c34bc20c231d70f006
Tim Tesner <ttesner+twitter@gmail.com>
2026-02-22 17:46:58 -0500

    Update sw.js
    v3

--- Gyro_Sound_Synth_2/sw.js
+++ Gyro_Sound_Synth_2/sw.js
@@ -1,12 +1,3 @@
-self.addEventListener('install', e => {
-  self.skipWaiting();
-  e.waitUntil(caches.open('gyros-v2').then(c => c.addAll(['index.html', 'manifest.json'])));
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(
-      keys.map(key => { if (key !== 'gyros-v2') return caches.delete(key); })
-  )));
-});
-
+self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open('gyros-v3').then(c => c.addAll(['index.html', 'manifest.json']))); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(k => Promise.all(k.map(key => { if (key !== 'gyros-v3') return caches.delete(key); })))); });
 self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
