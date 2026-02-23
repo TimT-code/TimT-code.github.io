a1bc6fb6ddcce9ed7377acb83562317c36462600
Tim Tesner <ttesner+twitter@gmail.com>
2026-02-23 00:26:04 -0500

    Create sw.js
    v6

---
+++ Gyro_Sound_Synth_6/sw.js
@@ -0,0 +1,3 @@
+self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open('gyros-v6').then(c => c.addAll(['index.html', 'manifest.json']))); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(k => Promise.all(k.map(key => { if (key !== 'gyros-v6') return caches.delete(key); })))); });
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
